6f233d4ca345934d6e447d944ed4ae6385e386ad
Corina <Corina@LAPTOP-UD9CV3NA>
2025-10-21 12:26:51 +0300

    New commit

--- Models/Book.cs
+++ Models/Book.cs
@@ -13,10 +13,11 @@ namespace Lazar_Corina_Lab2.Models
 
         [Column(TypeName = "decimal(6,2)")]
         public decimal Price { get; set; }
+
         [DataType(DataType.Date)]
         public DateTime PublishingDate { get; set; }
         public int? PublisherID { get; set; }
-        public Publisher? Publisher { get; set; }
+        public Publisher? Publisher { get; set; } // Navigation property
         // Foreign Key
         public int? AuthorID { get; set; }
 
