58512de46165f0aeb184dbc955dd8ce5d17bbf90
Corina <Corina@LAPTOP-UD9CV3NA>
2025-10-24 13:31:32 +0300

    Laborator 3 complet

--- Pages/Books/Details.cshtml.cs
+++ Pages/Books/Details.cshtml.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 using Lazar_Corina_Lab2.Data;
 using Lazar_Corina_Lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lazar_Corina_Lab2.Pages.Books
 {
@@ -20,24 +22,32 @@ namespace Lazar_Corina_Lab2.Pages.Books
         }
 
         public Book Book { get; set; } = default!;
+        public SelectList AuthorList { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? id)
+        [BindProperty(SupportsGet = true)]
+        public int? SelectedAuthorID { get; set; }
+        public BookData BookD { get; set; }
+        public int BookID { get; set; }
+        public int CategoryID { get; set; }
+        public async Task OnGetAsync(int? id, int? categoryID)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            BookD = new BookData();
 
-            var book = await _context.Book.FirstOrDefaultAsync(m => m.Id == id);
-            if (book == null)
+            //se va include Author conform cu sarcina de la lab 2
+            BookD.Books = await _context.Book
+            .Include(b => b.Publisher)
+            .Include(b => b.Author)
+            .Include(b => b.BookCategories)
+            .ThenInclude(b => b.Category)
+            .AsNoTracking()
+            .OrderBy(b => b.Title)
+            .ToListAsync();
+            if (id != null)
             {
-                return NotFound();
+                BookID = id.Value;
+                Book = BookD.Books.Single(i => i.Id == id.Value);
+                BookD.Categories = Book.BookCategories.Select(s => s.Category);
             }
-            else
-            {
-                Book = book;
-            }
-            return Page();
         }
     }
 }
--- Pages/Books/Index.cshtml.cs
+++ Pages/Books/Index.cshtml.cs
@@ -38,6 +38,7 @@ namespace Lazar_Corina_Lab2.Pages.Books
             //se va include Author conform cu sarcina de la lab 2
             BookD.Books = await _context.Book
             .Include(b => b.Publisher)
+            .Include(b => b.Author)
             .Include(b => b.BookCategories)
             .ThenInclude(b => b.Category)
             .AsNoTracking()
